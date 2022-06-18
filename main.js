2a6cd7116dd79b525e66d3337cec9a68c9e341ed
Letam Bossman Barinua <letambarinua@gmail.com>
2022-06-18 21:56:11 +0100

    Cache some DOM elements in JS file for Password comfirmation

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-console.log('Testing link up.')
+let password = document.querySelector('#password');
+let comfirmPassword = document.querySelector('#comfirm-password');
+const signUpButton = document.querySelector('button');
+
+comfirmPassword.addEventListener('keyup', () => {
+    let comfirmPwdArray = [];
+    comfirmPwdArray.push(comfirmPassword.value);
+    console.log(comfirmPwdArray);
+    if (password.value === comfirmPassword.value)console.log('It works: ${password.value}');
+});
